d60f4c439ffb552005c5c63e0820eeb483faea02
Franck Chastagnol <franckc@gmail.com>
2025-01-22 20:07:20 -0800

    Fix build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,11 +5,10 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  const globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient };
-  if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient();
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
   }
-  prisma = globalWithPrisma.prisma;
+  prisma = (global as any).prisma;
 }
 
 export default prisma;
