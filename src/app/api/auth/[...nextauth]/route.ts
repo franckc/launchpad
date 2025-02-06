8594e20e7652948efa93d81c89983346a14e1d83
Franck Chastagnol <franckc@gmail.com>
2025-02-05 22:33:25 -0800

    Plumbing for classification

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
+import { Session } from 'next-auth'
 
 
 if (!process.env.GOOGLE_ID) {
@@ -9,53 +10,41 @@ if (!process.env.GOOGLE_SECRET) {
   throw new Error("Missing GOOGLE_SECRET environment variable ");
 }
 
-
 const handler = NextAuth({
   providers: [
     // OAuth authentication provider(s)
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          scope: [
+            "openid",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/gmail.readonly",
+            "https://www.googleapis.com/auth/calendar.readonly"
+          ].join(" "),
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
   ],
+  // Capture the access token for use when calling the Google API
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token } : { session: any, token: any }) {
+      session.accessToken = token.accessToken;
+      return session;
+    }
+  }
 })
-
-// Use this Handler if needing to capture the access token to make API calls.
-// const handler = NextAuth({
-//   providers: [
-//     // OAuth authentication provider(s)
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//       authorization: {
-//         params: {
-//           scope: [
-//             "openid",
-//             "https://www.googleapis.com/auth/userinfo.email",
-//             "https://www.googleapis.com/auth/userinfo.profile",
-//             "https://www.googleapis.com/auth/gmail.readonly",
-//             "https://www.googleapis.com/auth/calendar.readonly"
-//           ].join(" "),
-//           prompt: "consent",
-//           access_type: "offline",
-//           response_type: "code"
-//         }
-//       }
-//     }),
-//   ],
-//   // Capture the access token for use when calling the Google API
-//   callbacks: {
-//     async jwt({ token, account }) {
-//       if (account) {
-//         token.accessToken = account.access_token;
-//       }
-//       return token;
-//     },
-//     async session({ session, token }) {
-//       session.accessToken = token.accessToken;
-//       return session;
-//     }
-//   }
-// })
 
 export { handler as GET, handler as POST }
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,10 +4,12 @@ import React from "react";
 import { AgentList } from "../(components)/agent-list";
 import { ConnectedServices } from "../(components)/connected-services";
 import { AgentInbox } from "../(components)/agent-inbox";
+import { Calendar } from "../(components)/calendar-card";
 
 export default function HomePage() {
   return (
     <div className="space-y-8">
+      <Calendar />
       <AgentList />
       <ConnectedServices />
       <AgentInbox />
